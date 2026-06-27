fbc4457d7ec32225c6980c9adc23ba592ffe1cd8
keshav verma <keshavcode1@gmail.com>
2026-06-27 13:17:03 +0530

    some changes and concept learning in vid-id-102

--- vid-id-101/src/myntra.js
+++ vid-id-101/src/myntra.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import reactDOM from "react-dom/client"
 import Card from "./component/card";
 import Header from "./component/header";
@@ -14,6 +14,19 @@ import {Greet as Goa} from "./utils/dummy";
 
 
 function App(){
+    let [A,setA]=useState(arr);
+
+    function sortArray(){
+        A.sort((a,b)=> a.price-b.price)
+        setA([...A])
+        console.log(A);
+        
+    }
+    function filter500(){
+        const B = arr.filter((value)=>value.price>499);
+        setA(B);
+    }
+
     return(
         <>
         {/* header */}
@@ -23,7 +36,8 @@ function App(){
         {/* // body */}
         
         <div className="upper-mid">
-            <button id="sort">sort</button>
+            <button onClick={sortArray} id="sort">sort by Price</button>
+            <button className="filter" onClick={filter500} >Price above 499</button>
         </div>
 
         <div className="middle" style={{display:"flex", gap:"20px", flexWrap:"wrap"}}>
@@ -32,7 +46,7 @@ function App(){
                 {/* return js expression array,react element,string and numbers  */}
 
             {
-                arr.map((val,index)=><Card key={index} cloth={val.cloth} offer={val.offer} price={val.price} />)
+                A.map((val,index)=><Card key={index} cloth={val.cloth} offer={val.offer} price={val.price} />)
 
                 // here map returns array which is acceptable here because in this block only something which is some returnable is accepted , 
                 // it's like [<card/>,<card/>,<card/>,<card/>,<card/>,<card/>,<card/>,<card/>,]
--- vid-id-101/src/utils/dummy.js
+++ vid-id-101/src/utils/dummy.js
@@ -1,4 +1,6 @@
-  const arr=[{cloth:"T-shirt",offer:"30-50%",price:"500"},{cloth:"Lower",offer:"70%",price:"300"},{cloth:"Pants",offer:"50%",price:"800"},{cloth:"Skirt",offer:"10%",price:"400"},{cloth:"Inner",offer:"30-50%",price:"500"},{cloth:"kurta",offer:"50%",price:"1100"},{cloth:"Underwear",offer:"30%",price:"150"}]
+  let arr=[{cloth:"T-shirt",offer:"30-50%",price:"500"},{cloth:"Lower",offer:"70%",price:"300"},{cloth:"Pants",offer:"50%",price:"800"},{cloth:"Skirt",offer:"10%",price:"400"},{cloth:"Inner",offer:"30-50%",price:"500"},{cloth:"kurta",offer:"50%",price:"1100"},{cloth:"Underwear",offer:"30%",price:"150"}]
+
+
 
 export function Greet(){
     return <h1>hello coder army</h1>
